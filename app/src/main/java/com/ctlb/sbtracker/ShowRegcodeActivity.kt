c12e87e5a3a770faa6e4d6c37e52ebf5e44f611e
charan kumar reddy Guggulla <charankumar.g5@gmail.com>
2020-11-16 15:29:25 +0530

    Update ShowRegcodeActivity.kt

--- app/src/main/java/com/ctlb/sbtracker/ShowRegcodeActivity.kt
+++ app/src/main/java/com/ctlb/sbtracker/ShowRegcodeActivity.kt
@@ -7,5 +7,7 @@ class ShowRegcodeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_regcode)
+        val ref_number: TextView = findViewById(R.id.reference_number)
+        ref_number.text="reference number"
     }
 }
